8ed12d604938622f0d1ee493c57ff8b020ddfd66
Kevin Reiss <kevin.reiss@gmail.com>
2023-03-02 11:28:56 -0600

    Update cap config to dump database in the app role.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,4 @@ server "recap-www-prod1", user: fetch(:user), roles: %w{app drupal_primary}
 set :search_api_solr_host, 'lib-solr.princeton.edu'
 set :search_api_solr_path, '/solr/recap-production'
 
-server "mysql-db-prod1", user: 'pulsys', roles: %w{db}
 set :db_name, "recap-prod"
