9141e16ef22b04d63e29507ca075b63f40b1a1f0
Francis Kayiwa <kayiwa@pobox.com>
2023-05-23 10:21:15 -0400

    add secondvm to deploy to

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,7 @@
-server "recap-www-staging1", user: fetch(:user), roles: %w{app drupal_primary}
+server 'recap-www-staging1', user: fetch(:user), roles: %w[app drupal_primary]
+server 'recap-www-staging2', user: fetch(:user), roles: %w[app drupal_primary]
 
 set :search_api_solr_host, 'lib-solr-staging.princeton.edu'
 set :search_api_solr_path, '/solr/recap-staging'
 
-set :db_name, "recap-staging"
+set :db_name, 'recap_staging'
