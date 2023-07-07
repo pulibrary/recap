3b5731cf5238ff33980b3fcdc09f5e6612ac3a67
Francis Kayiwa <kayiwa@pobox.com>
2023-07-07 13:51:21 -0400

    add the new jammy vm
    use a posix friendly db name

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,32 +21,8 @@ set :cas_cert_location, "/etc/ssl/certs/ssl-cert-snakeoil.pem"
 
 set :user, "deploy"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :keep_releases, 5
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
 namespace :drupal do
